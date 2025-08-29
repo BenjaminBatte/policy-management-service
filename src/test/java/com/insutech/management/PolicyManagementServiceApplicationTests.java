0a1ed1f1d620e6089b873e2c0cf35cfa9848c58b
Benjamin Batte <benjaminbatte@gmail.com>
2025-08-29 12:27:53 -0600

    first commit

---
+++ src/test/java/com/insutech/management/PolicyManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insutech.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
